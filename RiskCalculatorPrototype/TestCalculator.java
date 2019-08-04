670eed30e23dcbd388cb390f7454ebbcb0d712e4
Russel Arthur <concrain@gmail.com>
2019-08-03 22:36:43 -0700

    changed the way the stop-loss is calculated

---
+++ RiskCalculatorPrototype/TestCalculator.java
@@ -0,0 +1,24 @@
+
+import static org.junit.Assert.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * The test class TestCalculator.
+ *
+ * @author  (your name)
+ * @version (a version number or a date)
+ */
+public class TestCalculator {
+
+    @Before
+    public void setUp() {
+        
+    }
+
+    @After
+    public void tearDown() {
+        
+    }
+}
